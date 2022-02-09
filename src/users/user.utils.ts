afadeef8b47364949b7ffc123b4787c097dd460c
Eumericano <opwer032@naver.com>
2022-02-10 08:39:50 +0900

    refactor: Add signal type check logic on protected Resolver

--- src/users/user.utils.ts
+++ src/users/user.utils.ts
@@ -5,26 +5,29 @@ import { Resolver } from "@src/types/resolvers";
 import { Context } from "@src/types/context";
 
 export const getUserByAuth = async (token: string) => {
-    try {
-        if (!token) return null;
+  try {
+    if (!token) return null;
 
-        const { id } = jwt.verify(token, process.env.SECRET_KEY) as User;
-        const user = await client.user.findUnique({ where: { id } });
+    const { id } = jwt.verify(token, process.env.SECRET_KEY) as User;
+    const user = await client.user.findUnique({ where: { id } });
 
-        if (user) return user;
-        else return null;
-    } catch {
-        return null;
-    }
+    if (user) return user;
+    else return null;
+  } catch {
+    return null;
+  }
 };
 
 export const protectedResolver =
-    (resolver: Resolver) =>
-    (root: any, args: any, context: Context, info: any) => {
-        if (!context.loggedInUser)
-            return {
-                ok: false,
-                error: "로그인 후 이용할 수 있습니다.",
-            };
-        else return resolver(root, args, context, info);
-    };
+  (resolver: Resolver) =>
+  (root: any, args: any, context: Context, info: any) => {
+    if (!context.loggedInUser) {
+      const query = info.operation.operation == "query";
+      if (query) return null;
+      else
+        return {
+          ok: false,
+          error: "로그인 후 이용할 수 있습니다.",
+        };
+    } else return resolver(root, args, context, info);
+  };
